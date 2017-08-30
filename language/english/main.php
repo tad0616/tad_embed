9012adafc29f850b4a5b11f39d9e14aeea12c0e4
mambax7 <mambax7@gmail.com>
2017-08-30 01:35:54 -0400

    added English

---
+++ language/english/main.php
@@ -0,0 +1,10 @@
+<?php
+//需加入模組語系
+define('_TAD_NEED_TADTOOLS', 'This module needs TadTools module. You can download TadTools from <a href="http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50" target="_blank">Tad Textbook </a>. ');
+
+define("_MD_TADEMBED_SMNAME1", "embedded block");
+define("_MD_TADEMBED_COPY", "Please copy the source code below, paste the page you want to render.");
+define("_MD_TADEMBED_PREVIEW", "This is the effect preview (actually does not contain outer rounded box).");
+define("_MD_TADEMBED_SELECT_BLOCK", "Please select the block to be embedded");
+define("_MD_TADEMBED_DEMO", "render preview");
+define("_MD_TADEMBED_NOTE", "if the bottom of the blank, please check whether the blank_theme blank set.");
