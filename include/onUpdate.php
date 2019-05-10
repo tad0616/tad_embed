c81ca5b0c2fbdede6d68dcbf0df50fc2a3a0afb3
Tad <tad0616@gmail.com>
2019-05-10 16:09:36 +0800

    更新說明

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -2,6 +2,9 @@
 
 use XoopsModules\Tad_embed\Update;
 
+if (!class_exists('XoopsModules\Tad_embed\Update')) {
+    include dirname(__DIR__) . '/preloads/autoloader.php';
+}
 function xoops_module_update_tad_embed(&$module, $old_version)
 {
     global $xoopsDB;
